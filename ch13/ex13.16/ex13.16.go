0fa89ffc5fb5a1f763955a405ffe8935e8cc3b06
Vong Kong <tucker@naver.com>
2024-03-05 19:16:15 -0800

    must have Go 2

---
+++ ch13/ex13.16/ex13.16.go
@@ -0,0 +1,15 @@
+// ch15/ex15.16/ex15.16.go
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	var str string = "Hello World"
+	var slice []byte = []byte(str)
+
+	fmt.Printf("str:\t%p\n", unsafe.StringData(str))
+	fmt.Printf("slice:\t%p\n", unsafe.SliceData(slice))
+}
